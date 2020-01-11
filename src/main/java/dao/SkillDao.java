95eeeeac064cf1a2b5bf021b863d341ad0dccb66
Krzychu Sieradzki <krzysztofsieradzki00@gmail.com>
2020-01-11 16:25:06 +0100

    Dao update

---
+++ src/main/java/dao/SkillDao.java
@@ -0,0 +1,24 @@
+package dao;
+
+import model.Skill;
+import model.Source;
+import org.hibernate.SessionFactory;
+
+public class SkillDao extends BaseDao {
+    protected SkillDao(SessionFactory sessionFactory) {
+        super(sessionFactory);
+    }
+
+    public Skill get(Long id){
+        return super.produceInTransaction(session -> session.get(Skill.class,id));
+    }
+    public void save(Skill skill){
+        super.executeInTransaction(session -> session.save(skill));
+    }
+    public void delete(Skill skill){
+        super.executeInTransaction(session -> session.delete(skill));
+    }
+    public void update(Skill skill){
+        super.executeInTransaction(session -> session.update(skill));
+    }
+}
---
+++ src/main/java/dao/SourceDao.java
@@ -0,0 +1,23 @@
+package dao;
+
+import model.Source;
+import org.hibernate.SessionFactory;
+
+public class SourceDao extends BaseDao {
+    protected SourceDao(SessionFactory sessionFactory) {
+        super(sessionFactory);
+    }
+
+    public Source get(Long id){
+        return super.produceInTransaction(session -> session.get(Source.class,id));
+    }
+    public void save(Source source){
+        super.executeInTransaction(session -> session.save(source));
+    }
+    public void delete(Source source){
+        super.executeInTransaction(session -> session.delete(source));
+    }
+    public void update(Source source){
+        super.executeInTransaction(session -> session.update(source));
+    }
+}
