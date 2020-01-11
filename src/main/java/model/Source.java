b11c1b3c01a033be812427a1926b1986c0ac3fe9
Krzychu Sieradzki <krzysztofsieradzki00@gmail.com>
2020-01-11 13:55:39 +0100

    Model v 1.0

---
+++ src/main/java/model/Source.java
@@ -0,0 +1,61 @@
+package model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name="sources")
+public class Source {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column
+    private String description;
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Source source = (Source) o;
+        return id.equals(source.id) &&
+                description.equals(source.description) &&
+                name.equals(source.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, description, name);
+    }
+
+    @Override
+    public String toString() {
+        return "Source{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/model/User.java
@@ -0,0 +1,86 @@
+package model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column
+    private String first_name;
+    @Column
+    private String last_name;
+    @Column(nullable = false, unique = true)
+    private String password;
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id.equals(user.id) &&
+                first_name.equals(user.first_name) &&
+                last_name.equals(user.last_name) &&
+                password.equals(user.password) &&
+                username.equals(user.username);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, first_name, last_name, password, username);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", first_name='" + first_name + '\'' +
+                ", last_name='" + last_name + '\'' +
+                ", password='" + password + '\'' +
+                ", username='" + username + '\'' +
+                '}';
+    }
+}
